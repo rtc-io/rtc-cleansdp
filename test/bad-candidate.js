3f080dedfe300ea39846608e9601500fc5e67377
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-19 16:10:16 +1000

    Progress on test implementation

--- test/bad-candidate.js
+++ test/bad-candidate.js
@@ -1,1 +1,11 @@
 var test = require('tape');
+var fs = require('fs');
+var clean = require('..');
+
+var input = fs.readFileSync(__dirname + '/data/bad-candidate.txt', 'utf8');
+var expected = fs.readFileSync(__dirname + '/data/bad-candidate-clean.txt', 'utf8');
+
+test('a bad candidate is removed from the sdp', function(t) {
+  t.plan(1);
+  t.equal(clean(input), expected, 'matched expected output');
+});
